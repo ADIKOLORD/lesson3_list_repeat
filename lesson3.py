ff567fbb13c4f73a71f36eddfd3afef9bf41f986
Kyrgyzbaev ADILET <98408321+ADIKOLORD@users.noreply.github.com>
2022-07-14 19:23:32 +0600

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -11,7 +11,7 @@ while True:
         job = input('Your job: ')
     if name != '' and age != '' and job != '':
         break
-'''aaaafaffaaaaaaff
+'''aaaafaffaaaaaafaf
 ffffaaaafffafffffa
 '''
 # Problem 1
