14a3e1a2f2dd83c799af02bfdcdd3ce86f1dd0f4
Kyrgyzbaev ADILET <98408321+ADIKOLORD@users.noreply.github.com>
2022-07-14 19:22:45 +0600

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -12,7 +12,7 @@ while True:
     if name != '' and age != '' and job != '':
         break
 '''aaaafaffaaaaaa
-ffffaaaafff
+ffffaaaafffa
 '''
 # Problem 1
 languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
